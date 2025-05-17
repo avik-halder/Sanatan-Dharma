429ab5556d24e57ee7f39b805318d3a6abd4a8bc
Avik Halder <official.avik.h@gmail.com>
2025-05-17 15:27:27 +0600

    ok

--- src/pages/deities/Ganesha.jsx
+++ src/pages/deities/Ganesha.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
-import ganesh from "../../assets/Deities/ganesh.webp";
+import ganesh from "../../assets/Deities/Ganesh.webp";
 import ganeshCaturthi from "../../assets/Festivals/ganesh chaturthi.webp";
 
 import birth from '../../assets/Deities/Ganesh/Birth.jpg'
